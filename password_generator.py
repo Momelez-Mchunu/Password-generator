7e8910be2f452413078b94d3afed2bdd59f57211
unknown <kmchunu029@gmail.com>
2021-12-03 00:17:26 +0200

    generate password button

--- password_generator.py
+++ password_generator.py
@@ -5,4 +5,21 @@ from random import randint
 root = Tk()
 root.title('Password generator')
 root.geometry('500x300')
+
+labelFrame =  LabelFrame(root, text="How many characters?")
+labelFrame.pack(pady=20)
+
+entry = Entry(labelFrame,font=("Helvetica",24))
+entry.pack(pady=20,padx=20)
+
+password_entry = Entry(root, text="",font=("Helvetica",24))
+password_entry.pack(pady=20)
+
+my_frame = Frame(root)
+my_frame.pack(pady=20)
+
+generate_button = Button(my_frame,text="Generate password")
+generate_button.grid(row=0,column=0,padx=10)
+
+
 root.mainloop()
