c93d56fa2f3c450bc6f67676709ffa04253f4018
unknown <kmchunu029@gmail.com>
2021-12-03 00:20:14 +0200

    copy to clipboard button

--- password_generator.py
+++ password_generator.py
@@ -16,10 +16,12 @@ password_entry = Entry(root, text="",font=("Helvetica",24))
 password_entry.pack(pady=20)
 
 my_frame = Frame(root)
+
 my_frame.pack(pady=20)
 
 generate_button = Button(my_frame,text="Generate password")
 generate_button.grid(row=0,column=0,padx=10)
 
-
+clip_button =  Button(my_frame,text="Copy to clipboard")
+clip_button.grid(row=0,column=1,padx=10)
 root.mainloop()
