109ca5ab317c5ae9a1a43bfb92374c388ebb1ed6
unknown <kmchunu029@gmail.com>
2021-12-03 00:40:58 +0200

    generate password method

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,15 @@
-from tkinter import *
-from random import randint
+from tkinter import Entry,LabelFrame,Frame,Button,Tk
+from random import choice as choice
+from tkinter.constants import END
 
-
+def gen_password():
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@$%^&*()~`-_.,?1234567890'
+    password_entry.delete(0,END)
+    pass_length = int(entry.get())
+    password=""
+    for x in range(pass_length):
+        password +=choice(chars)
+    password_entry.insert(0,password)
 root = Tk()
 root.title('Password generator')
 root.geometry('500x300')
@@ -19,9 +27,10 @@ my_frame = Frame(root)
 
 my_frame.pack(pady=20)
 
-generate_button = Button(my_frame,text="Generate password")
+generate_button = Button(my_frame,text="Generate password",command=gen_password)
 generate_button.grid(row=0,column=0,padx=10)
 
 clip_button =  Button(my_frame,text="Copy to clipboard")
 clip_button.grid(row=0,column=1,padx=10)
+
 root.mainloop()
